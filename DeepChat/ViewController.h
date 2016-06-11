6c0deb5db3bbb1b258c7b1d00feff462936ea532
dat <dat@macbook-pro.local>
2016-06-11 03:07:06 -0700

    initial files

---
+++ DeepChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeepChat
+//
+//  Created by dat on 6/11/16.
+//  Copyright © 2016 codeDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DeepChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DeepChat
+//
+//  Created by dat on 6/11/16.
+//  Copyright © 2016 codeDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
